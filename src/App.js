9c4340ef0973a2d666dc80afe27dbd28e9c04523
JaishreeNishad <Jaysh2222@gmail.com>
2023-08-23 13:44:09 +0530

    Create Navbar using Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Header } from './components/Header';
 
 function App() {
   return (
     <>
-    hello
+    <Header/>
     </>
   );
 }
