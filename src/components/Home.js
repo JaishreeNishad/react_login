55e7dc88b31cce1bf1a50c22cd90bd4326b6416a
JaishreeNishad <Jaysh2222@gmail.com>
2023-08-23 17:01:56 +0530

    Provide name for all Form.control Field in Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -26,20 +26,20 @@ const Home = () => {
             <h3 className="text-center col-lg-6">Sign Up</h3>
             <Form>
               <Form.Group className="mb-3 col-lg-6"  controlId="formBasicEmail">
-                <Form.Control type="email" onChange={getdata} placeholder="Enter Your Name" />
+                <Form.Control type="email" name='name' onChange={getdata} placeholder="Enter Your Name" />
               </Form.Group>
               <Form.Group className="mb-3 col-lg-6"  controlId="formBasicEmail">
-                <Form.Control type="email"  onChange={getdata}  placeholder="Enter Your Email" />
+                <Form.Control type="email" name=' email' onChange={getdata}  placeholder="Enter Your Email" />
               </Form.Group>
 
               <Form.Group
                 className="mb-3 col-lg-6"
                 controlId="formBasicPassword"
               >
-                <Form.Control type="password"  onChange={getdata} placeholder="Password" />
+                <Form.Control type="password" name='password'  onChange={getdata} placeholder="Password" />
               </Form.Group>
               <Form.Group className="mb-3 col-lg-6"   controlId="formBasicEmail">
-                <Form.Control type="Date" onChange={getdata}/>
+                <Form.Control type="Date" name='date' onChange={getdata}/>
               </Form.Group>
               <Form.Group className="mb-3"  controlId="formBasicCheckbox">
                 <Form.Check type="checkbox"   onChange={getdata} label="Check me out" />
